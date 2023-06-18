8710232dc53ec36575d06fee85c7913918d8f3a0
Naem-Nizam <nizamuddin15bd@gmail.com>
2023-06-18 20:36:52 +0600

    my firt commit

---
+++ --setps.js
@@ -0,0 +1,48 @@
+/*
+ * 1.Create a node server with 5 steps
+ * 1.1. create folder
+ * 1.2. npm init -y
+ * 1.3. npm i express cors mongodb
+ * 1.4. script-dev: nodemon index.js
+ * 1.5. create index.js
+ * 1.6. use 5 steps to create a node server
+ *
+ *
+ * ------------------------------
+ * Create Atlas Account
+ * ------------------------------
+ * 1. Sign up. google access
+ * 2. create cluster
+ * 3. Create user dbUser and password
+ * 4. Network A/**ccess --> ip address: allow all
+ * 5. database > Connect > code copy pasta in index.js
+ *
+ * ----------------------------
+ * CRUD Operation
+ * ----------------------------
+ * 1. node mongodb CRUD > fundamentals
+ * 2.create async run function
+ * ----------------------------------
+ * Integrate sending data from client to server
+ * ------------------------------------
+ * 1. Client side: create form
+ * 2. on submit get form data and create user object
+ * 3. on server: Create user POST method to receive data on the backend
+ * 4. on client side: set fetch with POST, headers, body
+ * 5. Make sure you return a json from the POST API
+ *
+ * ----------------------------------
+ * LOAD Date to the client side
+ * -------------------------------
+ * 1. create get API on te server
+ * 2. create a query object query
+ * 3. collection.find (query)
+ * 4. cursor.toArray()
+ * 5. return the result
+ * 6.from client userEffect and display like you have done before
+ * --------------------------------------
+ * Delete
+ * ---------------------------------------
+ *
+ *
+ **/
